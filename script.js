25e31bfc9f8b2a1f901b5351a899dbec50556bfd
Anderson <asn@vortx.com.br>
2023-07-24 10:41:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,210 @@
+const faixas = [
+    { limite: 6677.55, aliquota: 0.0, parcelaDeduzir: 0 },
+    { limite: 9922.28, aliquota: 0.075, parcelaDeduzir: 500.82 },
+    { limite: 13167, aliquota: 0.15, parcelaDeduzir: 1244.99 },
+    { limite: 16380.38, aliquota: 0.225, parcelaDeduzir: 2232.51 },
+    { limite: Infinity, aliquota: 0.275, parcelaDeduzir: 3051.53 }
+  ];
+
+
+function formatarValor(element) {
+    const valor = element.value.replace(/\D/g, ''); // Remove caracteres não numéricos
+
+    const valorFormatado = (parseFloat(valor) / 100).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    element.value = valorFormatado;
+}
+
+
+function calculaPLR_semestre_1() {
+
+    let plrInput = document.getElementById("plr1").value;
+    let plr1 = parseFloat(plrInput.replace(/\D/g, '').replace(',', '.')) / 100;
+
+    // Verifica se o valor é válido
+    if (isNaN(plr1) || plrInput.trim() === '' || plr1 === 0) {
+        alert("Digite o valor bruto da primeira parcela de PLR");
+        return;
+    }
+
+    // Iterar sobre as faixas e encontrar a correspondente ao valor do PLR
+    for (const faixa of faixas) {
+        if (plr1 <= faixa['limite']) {
+        aliquota1 = faixa['aliquota'];
+        deducao1 = faixa['parcelaDeduzir'];
+        break;
+        }
+    }
+
+
+    let impostoDeRenda1 = (plr1 * aliquota1) - deducao1; // imposto a pagar
+    let plrLiquido1 = plr1 - impostoDeRenda1; // imposto a pagar - plr bruto
+
+    // Formata o resultado do PLR líquido
+    let resultadoFormatado = (plrLiquido1).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    // Formata o resultado da deducao
+    let deducaoFormatada = (deducao1).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+
+    // Formata o resultado do IR
+    let impostoFormatado = (impostoDeRenda1).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 3,
+        maximumFractionDigits: 3
+    });
+
+    // Exibe o resultado na página
+    document.getElementById("resultado1").innerHTML = resultadoFormatado;
+    document.getElementById("parcelaSpan").innerHTML = deducaoFormatada;
+    document.getElementById("aliquotaSpan").innerHTML =  `${(aliquota1 * 100).toFixed(2)}%`;
+    document.getElementById("imposto-renda1").innerHTML = impostoFormatado;
+
+    // Exibe o resultado total 
+
+    // Formata o plr 1
+    let plrFormatado = (plr1).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+
+    document.getElementById("plr-periodo-1").innerHTML = plrFormatado;
+    document.getElementById("plr-total").innerHTML = plrFormatado;
+    document.getElementById("imposto-renda-total").innerHTML = impostoFormatado;
+    document.getElementById("plr-liquido-total").innerHTML = resultadoFormatado;
+    
+
+    return [plr1, impostoDeRenda1, plrLiquido1];
+}
+
+
+
+function calculaPLR_semestre_2() {
+
+    // PLR bruto 1
+    let primeiroPLR_input = document.getElementById("plr1").value;
+    let plr1 = parseFloat(primeiroPLR_input.replace(/\D/g, '').replace(',', '.')) / 100;
+
+    // PLR bruto 2
+    let segundoPLR_input = document.getElementById("plr2").value;
+    let plr2 = parseFloat(segundoPLR_input.replace(/\D/g, '').replace(',', '.')) / 100;
+
+    console.log(plr2)
+    // Verifica se o valor é válido
+    if (isNaN(plr2) || segundoPLR_input.trim() === '' || plr2===0) {
+        alert("Digite o valor bruto da segunda parcela de PLR");
+        return;
+    }
+
+
+    impostoDeRenda1 = calculaPLR_semestre_1()[1];
+    plrLiquido1 = calculaPLR_semestre_1()[2];
+
+    let plrTotal = plr1 + plr2;
+
+    // Iterar sobre as faixas e encontrar a correspondente ao valor do PLR
+    for (const faixa of faixas) {
+        if (plrTotal <= faixa['limite']) {
+        aliquota2 = faixa['aliquota'];
+        deducao2 = faixa['parcelaDeduzir'];
+        break;
+        }
+    }
+    
+    let impostoDeRendaTotal = (plrTotal * aliquota2) - deducao2; // imposto a pagar
+    let impostoDeRenda2 = impostoDeRendaTotal - impostoDeRenda1; // imposto a pagar - imposto pago na parcela 1
+    let plrLiquido2 = plr2 - impostoDeRenda2; // plr bruto 2 - imposto real = plr líquida
+
+    // Exibe o resultado na página
+    let resultadoFormatado = (plrLiquido2).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    // Exibe o resultado na página
+    let deducaoFormatada = (deducao2).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    // Exibe o resultado na página
+    let impostoFormatado = (impostoDeRenda2).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 3,
+        maximumFractionDigits: 3
+    });
+
+
+
+    document.getElementById("resultado2").innerHTML = resultadoFormatado;
+    document.getElementById("parcelaSpan2").innerHTML = deducaoFormatada;
+    document.getElementById("aliquotaSpan2").innerHTML =  `${(aliquota2 * 100).toFixed(2)}%`;
+    document.getElementById("imposto-renda2").innerHTML = impostoFormatado;
+
+    // totais
+    
+    // Formata o plr 2
+
+    irAnual = impostoDeRenda1 + impostoDeRenda2;
+    plrLiquidoAnual = plrLiquido1 + plrLiquido2;
+
+    let plrFormatado = (plr2).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    let plrTotalFormatado = (plrTotal).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    let impostoAnualFormatado = (irAnual).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+    let plrLiquidoAnualFormatado = (plrLiquidoAnual).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+
+
+    document.getElementById("plr-periodo-2").innerHTML = plrFormatado;
+    document.getElementById("plr-total").innerHTML = plrTotalFormatado;
+    document.getElementById("imposto-renda-total").innerHTML = impostoAnualFormatado;
+    document.getElementById("plr-liquido-total").innerHTML = plrLiquidoAnualFormatado;
+
+    return [plr2, impostoDeRenda2];
+}
